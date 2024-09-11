1ace487068b7adf34f7fc96652e5c2d092f27e3e
Enrique Ponce <101468277+CoyoKike@users.noreply.github.com>
2024-09-11 09:28:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 from pandas_profiling import ProfileReport
 import pandas as pd
 import os
+from openpyxl import load_workbook
+import openpyxl
 
 app = Flask(__name__)
 
@@ -17,23 +19,25 @@ def process_file():
         if file_ext not in [".csv", ".xlsx"]:
             return "Invalid file format. Please upload a CSV or XLSX file."
 
-        # Read the file into a DataFrame
         try:
             if file_ext == ".csv":
                 df = pd.read_csv(uploaded_file, encoding='utf-8')
             else:
                 df = pd.read_excel(uploaded_file)
         except UnicodeDecodeError:
-            # Try different encodings if UTF-8 fails
             try:
-                uploaded_file.seek(0)  # Reset file pointer to start
+                uploaded_file.seek(0)
                 df = pd.read_csv(uploaded_file, encoding='latin1')
             except UnicodeDecodeError:
                 try:
-                    uploaded_file.seek(0)  # Reset file pointer to start
+                    uploaded_file.seek(0)
                     df = pd.read_csv(uploaded_file, encoding='iso-8859-1')
                 except UnicodeDecodeError:
                     return "Unable to read the file due to encoding issues."
+        except TypeError as e:
+            return f"Error processing Excel file: {e}"
+        except openpyxl.utils.exceptions.InvalidFileException:
+            return "The Excel file format is not supported."
 
         # Replace spaces with underscores in categorical columns
         for col in df.select_dtypes(include=['object']).columns:
@@ -50,3 +54,4 @@ def process_file():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
